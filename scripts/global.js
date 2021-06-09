dcc9dbeaa663493a08f1e654d5d64ccf1e1498e5
HarsahilSingh <80291458+HarsahilSingh@users.noreply.github.com>
2021-06-10 09:58:03 +1200

    abc

--- scripts/global.js
+++ scripts/global.js
@@ -40,59 +40,8 @@ $(document).ready(function () {
         $('[data-toggle="popover"]').popover();
       });
       
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle="blue";
-    ctx.fillRect(35, 00, 60, 40);
    
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle="blue";
-    ctx.fillRect(45, 75, 55, 90);
    
-  
-
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle="blue";
-    ctx.fillRect(15, 320, 75, 60);
-
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle="blue";
-    ctx.fillRect(150, 340, 85, 60);
-
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle="blue";
-    ctx.fillRect(155, 205, 60, 50);
-
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle="blue";
-    ctx.fillRect(300, 0, 60, 40);
-
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle="blue";
-    ctx.fillRect(155, 83, 60, 55);
-
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle="blue";
-    ctx.fillRect(275, 85, 85, 65);
-   
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle="blue";
-    ctx.fillRect(275, 218, 85, 68);
-   
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.fillStyle="blue";
-    ctx.fillRect(275, 355, 80, 68);
-   
-
     
 })
 
