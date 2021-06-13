b0cc87c5a1ef787a54d91d2407f1dfa1de1b8d7c
noutashm <mojeeb.noutash@gmail.com>
2021-06-14 10:29:47 +1200

    progress

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,14 @@
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+window.requestAnimationFrame = requestAnimationFrame;
 var score = 0
 var wormHorde = []
 var playerImg = new Image()
 playerImg.src = "../images/player.png"
 var sad = new Audio('../sound/sad.wav')
 var happy = new Audio('../sound/happy.wav')
+var trackMovement = 0
+var keys = []
+var fps = 20
 
 //Canvas
 const c = document.getElementById("canvas")
@@ -20,6 +25,7 @@ var player = {
     y: c.height / 2,
     vx: 0,
     vy: 0,
+    speed: 6,
     width: sheet.width / 4,
     height: sheet.height / 4
 }
@@ -27,8 +33,8 @@ var player = {
 var worm = {
     x: getRandomInt(12, c.width - 12),
     y: getRandomInt(170, c.height),
-    vx: getRandomInt(-1, 1),
-    vy: getRandomInt(-1, 1),
+    vx: getRandomInt(-3, 3),
+    vy: getRandomInt(-3, 3),
     radius: 10,
     lifeCycle: 0
 }
@@ -37,45 +43,10 @@ var currentFrame = 0
 
 window.addEventListener("keydown", keysPressed, false)
 window.addEventListener("keyup", keysReleased, false)
-var trackMovement = 0
-var keys = []
 
 function keysPressed(e) {
     keys[e.keyCode] = true
-
-    if (keys[37] || keys[65] && player.x >= 0) { // left
-        player.x -= 4
-        trackMovement = 1
-    }
-
-    if (keys[38] || keys[87] && player.y >= 110) { // up
-        player.y -= 4
-        trackMovement = 3
-    }
-
-    if (keys[39] || keys[68] && player.x <= (c.width - player.width + 5)) { // right
-        player.x += 4
-        trackMovement = 2
-    }
-
-    if (keys[40] || keys[83] && player.y <= (c.height - player.height)) { // down
-        player.y += 4
-        trackMovement = 0
-    }
-
-    if (keys[32]) { //space - action
-        // if (hypot(zombie.x - shot.x, zombie.y - shot.y) < 10) {
-        //     zombieHorde.splice(i, 1)
-        //     shots.splice(j, 1)
-        //     spawnZombie()
-        //     spawnZombie()
-        //     score++
-        // }
-        sad.play()
-        //TODO: what happens if space is hit?
-    }
     e.preventDefault()
-    draw()
 }
 
 function keysReleased(e) {
@@ -83,8 +54,34 @@ function keysReleased(e) {
     trackMovement = 0
 }
 
-function updateFrame() {
-    ctx.clearRect(0, 0, c.width, c.height)
+function controls() {
+    if (keys[37] || keys[65] && player.x >= 0) { // left
+        player.x -= player.speed
+        trackMovement = 1
+    }
+
+    if (keys[38] || keys[87] && player.y >= 110) { // up
+        player.y -= player.speed
+        trackMovement = 3
+    }
+
+    if (keys[39] || keys[68] && player.x <= (c.width - player.width + 5)) { // right
+        player.x += player.speed
+        trackMovement = 2
+    }
+
+    if (keys[40] || keys[83] && player.y <= (c.height - player.height)) { // down
+        player.y += player.speed
+        trackMovement = 0
+    }
+
+    if (keys[32]) { //space - action
+        sad.play()
+    }
+}
+
+function animate() {
+    //player
     currentFrame = ++currentFrame % 4
     player.vx = currentFrame * player.width
     player.vy = 3
@@ -103,6 +100,7 @@ function updateFrame() {
             break
     }
 
+    //worm - lifecycle
     switch (worm.lifeCycle) {
         case 0:
             worm.x += worm.vx
@@ -113,7 +111,7 @@ function updateFrame() {
         case 1:
             worm.x += worm.vx
             worm.y += worm.vy
-            worm.radius += 0.1
+            worm.radius += 1
             if (worm.radius > 30) {
                 worm.lifeCycle = 2
             }
@@ -122,7 +120,7 @@ function updateFrame() {
         case 2:
             worm.x += worm.vx
             worm.y += worm.vy
-            worm.radius -= 0.1
+            worm.radius -= 1
             if (worm.radius < 1) {
                 worm.lifeCycle = 3
             }
@@ -136,89 +134,27 @@ function updateFrame() {
     }
 }
 
-// class Worm {
-//     constructor(x, y, vx, vy, radius) {
-//         this.x = x
-//         this.y = y
-//         this.vx = vx
-//         this.vy = vy
-//         this.radius = radius
-//         this.lifeCycle = 0
-
-//         this.draw = this.draw.bind(this)
-//         this.update = this.update.bind(this)
-//         this.timeout = this.timeout.bind(this)
-//         setTimeout(this.timeout, getRandomInt(1000, 10000))
-//     }
-
-//     timeout() {
-//         this.enabled = true
-//     }
-
-//     draw() {
-//         var grad = ctx.createRadialGradient(300, 100, 0, 300, 100, 316.23)
-//         grad.addColorStop(0, 'rgba(255, 255, 255, 1)')
-//         grad.addColorStop(1, 'rgba(255, 231, 132, 1)')
-
-//         ctx.beginPath()
-//         ctx.fillStyle = grad
-//         ctx.arc(this.x, this.y, this.radius, Math.PI, 0, false)
-//         ctx.fill()
-//         ctx.lineWidth = 1
-//         ctx.strokeStyle = 'black'
-//         ctx.closePath()
-//         ctx.stroke()
-//     }
-
-//     update() {
-//         if (this.enabled == false) {
-//             return
-//         }
-
-//         switch (this.lifeCycle) {
-//             case 0:
-//                 this.x += this.vx
-//                 this.y += this.vy
-//                 this.lifeCycle = 1
-
-//                 break
-//             case 1:
-//                 this.x += this.vx
-//                 this.y += this.vy
-//                 this.radius += 0.1
-//                 if (this.radius > 30) {
-//                     this.lifeCycle = 2
-//                 }
-
-//                 break
-//             case 2:
-//                 this.x += this.vx
-//                 this.y += this.vy
-//                 this.radius -= 0.1
-//                 if (this.radius < 1) {
-//                     this.lifeCycle = 3
-//                 }
-
-//                 break
-//             case 3:
-//                 this.x = getRandomInt(50, 950)
-//                 this.y = getRandomInt(250, 600)
-//                 this.lifeCycle = 0
-//                 break
-//         }
-
-//     }
-// }
+function updateFrame() {
+    controls()
+    ctx.clearRect(0, 0, c.width, c.height)
+    draw()
+    animate()
+    setTimeout(() => {
+        requestAnimationFrame(updateFrame)
+    }, 1000 / fps);
+}
 
 function draw() {
-    updateFrame()
     ctx.drawImage(playerImg, player.vx, player.vy, player.width, player.height, player.x, player.y, player.width, player.height)
+    spawnWorm(worm.x, worm.y, worm.radius)
+}
 
-    spawnWorm(getRandomInt(12, c.width - 12), getRandomInt(170, c.height), 10)
+function checkCollision() {
+
 }
 
 function spawnWorm(x, y, r) {
-    var grad = ctx.createRadialGradient(300, 100, 0, 300, 100, 316.23)
+    var grad = ctx.createRadialGradient(x, y, r, Math.PI, 0, 316.23)
     grad.addColorStop(0, 'rgba(255, 255, 255, 1)')
     grad.addColorStop(1, 'rgba(255, 231, 132, 1)')
     ctx.beginPath()
@@ -231,29 +167,12 @@ function spawnWorm(x, y, r) {
     ctx.stroke()
 }
 
-function startGame() {
-
-    setInterval(() => {
-        draw()
-    }, 200)
-    //window.requestAnimationFrame(drawPlayer)
+window.onload = function startGame() {
+    updateFrame()
 }
 
 function restartGame() {
     wormHorde = []
-}
-
-function gameLoop(time) {
-    timeoffset = time
-    if (time > endTime && endTime > 0) {
-        if (ScoreBoard !== null)
-            ScoreBoard.drawEnd(myGameArea.context)
-    }
-    else {
-        updateGameArea()
-        draw()
-    }
-    window.requestAnimationFrame(gameLoop)
 }
 
 function hypot(length1, length2) {
